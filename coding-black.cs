a4ec09aa7e44174c22e30e0a00c2baf32adc7944
black <black@example.com>
2025-10-20 20:21:54 +0500

    change message to hello everyone

--- coding-black.cs
+++ coding-black.cs
@@ -5,7 +5,7 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("hello");
+        Console.WriteLine("hello everyone");
     }
     public static void greetWhite()
     {
